77b74155b0ce32ece75980d893a665374cefe1cf
lloydst <lloyd.stumpel@gmail.com>
2017-08-22 09:20:24 +0200

    fixed some port inconsistencies

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var swaggerDefinition = {
     version: '1.0.0',
     description: 'Demonstrating how to describe a RESTful API with Swagger',
   },
-  host: 'localhost:3001',
+  host: 'localhost:3000',
   basePath: '/',
 };
 
